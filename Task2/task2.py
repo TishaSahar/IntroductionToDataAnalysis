5b607c28b90c2da77f7820e6b5c82c8d034fb9d0
TishaSahar <andreev.tihon@gmail.com>
2024-04-26 20:54:51 +0300

    Implemented task2.1

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,8 +1,8 @@
 import pandas as pd
-
 import xlrd
-
 import sys
+
+# Internal methods reusage 
 sys.path.append("C:/Users/andre/OneDrive/Documents/M.A/DA/IntroductionToDataAnalysis")
 import Task1.task1
 
@@ -12,7 +12,6 @@ def all_data(files):
     
     print("\nConstruct dictionary from files:\n", files)
     column_names = {}
-    column_types = {}
     data = []
     for file in files:
         book = xlrd.open_workbook(file)
@@ -25,14 +24,6 @@ def all_data(files):
         for col in range(0, ncols):
             column_names[col] = sheet.cell(0, col).value
 
-        # Get type of columns
-        # xl_cell_types = {xlrd.XL_CELL_EMPTY: "empty", xlrd.XL_CELL_TEXT: "text",
-        #                  xlrd.XL_CELL_NUMBER: "number", xlrd.XL_CELL_DATE: "date",
-        #                  xlrd.XL_CELL_BOOLEAN: "boolean", xlrd.XL_CELL_ERROR: "error"}
-
-        for col in range(0, ncols):
-            # column_types[column_names.get(col)] = xl_cell_types.get(sheet.cell(1, col).ctype)
-            column_types[column_names.get(col)] = type(sheet.cell(1, col).value)
         for row in range(1, nrows - 1):
 
             row_data = []
@@ -40,15 +31,14 @@ def all_data(files):
                 row_data.append(sheet.cell(row, col).value)
             data.append(row_data)
 
-    return column_types, column_names, data
+    return column_names, data
 
 def main():
     path_name = "C:/Users/andre/OneDrive/Documents/M.A/DA/IntroductionToDataAnalysis/Task2/Dannye_dlya_Praktiki_2_chast_2"
     
     all_files = Task1.task1.get_all_files(path_name)
-    types, columns, data = all_data(all_files)
+    columns, data = all_data(all_files)
     print("\n\t-> Column names and inexes:\n", columns)
-    print("\n\t-> Types:\n", types)
     print("\n\t-> All data:\n", pd.Series(data))
 
 if __name__ == "__main__":
---
+++ Task2/types_of_columns.md
@@ -0,0 +1,20 @@
+### Task 2
+
+# 1. Types of column data:
+    - City - номинальный
+    - State - номинальный
+    - Zipcode - дискретный
+    - Wheather Timestemp - непрерывный
+    - Temperature - непрерывный
+    - Wind_Chill(F) - непрерывный
+    - Humidity(%) - относительный
+    - Pressure(in) - непрерывный
+    - Visibility(mi) - дискретный
+    - Wind_Direction - номинальный
+    - Wind_Speed(mph) - непрерывный
+    - Precipitation(in) - непрерывный
+    - Weather_Condition - номинальный
+    - [Bump	Crossing, Give_Way, Junction, No_Exit, Railway, Roundabout, Station, Stop, Traffic_Calming, Traffic_Signal, Turning_Loop, Civil_Twilight] - номинальный
+    
+    
+    
