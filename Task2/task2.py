db031b36d462b42624414107501f778af475e801
TishaSahar <andreev.tihon@gmail.com>
2024-04-26 21:53:20 +0300

    Implemented matplot visualization of distributions for serverity.

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import xlrd
 import sys
+
+from collections import Counter
 
 # Internal methods reusage 
 sys.path.append("C:/Users/andre/OneDrive/Documents/M.A/DA/IntroductionToDataAnalysis")
@@ -13,6 +16,8 @@ def all_data(files):
     print("\nConstruct dictionary from files:\n", files)
     column_names = {}
     data = []
+    severity_list = []
+    visibility_list = []
     for file in files:
         book = xlrd.open_workbook(file)
         sheet = book.sheet_by_index(0)
@@ -22,24 +27,47 @@ def all_data(files):
         print("\nRanges: ", nrows, " ", ncols)
         # Indexes of columns
         for col in range(0, ncols):
-            column_names[col] = sheet.cell(0, col).value
+            column_names[sheet.cell(0, col).value] = col
 
         for row in range(1, nrows - 1):
-
+            severity_list.append(int(sheet.cell(row, int(column_names.get("Severity"))).value))
+            if sheet.cell(row, int(column_names.get("Visibility(mi)"))).value != "":
+                visibility_list.append(float(sheet.cell(row, int(column_names.get("Visibility(mi)"))).value))
             row_data = []
             for col in range(0, ncols):
                 row_data.append(sheet.cell(row, col).value)
             data.append(row_data)
 
-    return column_names, data
+    return column_names, data, severity_list, visibility_list
 
 def main():
     path_name = "C:/Users/andre/OneDrive/Documents/M.A/DA/IntroductionToDataAnalysis/Task2/Dannye_dlya_Praktiki_2_chast_2"
     
     all_files = Task1.task1.get_all_files(path_name)
-    columns, data = all_data(all_files)
+    columns, data, severity_list, visibility_list = all_data(all_files)
     print("\n\t-> Column names and inexes:\n", columns)
     print("\n\t-> All data:\n", pd.Series(data))
+    
+    # Sevirity type distribution plot
+    sevirity_types_counter = Counter(severity_list)
+    plt.bar(sevirity_types_counter.keys(), sevirity_types_counter.values())
+    plt.xlabel("Тип тяжести")
+    plt.ylabel("Количество наблюдений события")
+    plt.title("Распределение событий по типу")
+    #plt.show()
+
+    severity_list.sort()
+    sevirity_types_counter = Counter(severity_list)
+    sum = 0
+    summary_distribution = {}
+    for count in sevirity_types_counter.values():
+        sum += count
+        summary_distribution[sum] = sum
+    plt.bar(sevirity_types_counter.keys(), summary_distribution)
+    plt.xlabel("Тип тяжести по возрастанию")
+    plt.ylabel("Накопленное количество наблюдений события")
+    plt.title("Распределение событий по типу")
+    plt.show()
 
 if __name__ == "__main__":
     main()
